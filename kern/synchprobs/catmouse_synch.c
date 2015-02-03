cc66ce95d2199ce0a7dd3973f2ee8fcb7277c67d
nfrasser <nfrasser@gmail.com>
2015-02-03 14:58:35 -0500

    Some extra comments and documentation

--- kern/synchprobs/catmouse_synch.c
+++ kern/synchprobs/catmouse_synch.c
@@ -16,6 +16,12 @@ static struct lock *speciesSwitch;
 static struct lock *myBowl;
 static struct cv *myBowlIsAvailable;
 
+/**
+	A species is a structure containing some general information about a
+	particular species (mainly cats and mice).
+
+	It's also used to communicate with other species
+*/
 struct species {
 
 	// Are there members of this species that want to eat?
@@ -140,56 +146,80 @@ void catmouse_sync_cleanup(int bowls) {
 	}
 }
 
-void before_eating(struct species *mySpecies, struct species *otherSpecies, unsigned int bowl) {
+/**
+	A creature must call this if it wants to eat. Params are
 
-	// Don't queue any more of this creature if the opposite creature wants to eat
+	* Species of creature
+	* Species of other creature
+	* Bowl from which this creature wants to eat
+
+	To the possible kinds of species arbitrary (e.g., cats, dogs, rabbits, etc.)
+	with the same rule that only one type of species can eat from the bowl at a
+	time, "otherSpecies" would have to be every other species but this one.
+*/
+void before_eating(struct species *mySpecies, struct species *otherSpecies, unsigned int bowl) {
 
 	lock_acquire(speciesSwitch);
 		while (otherSpecies->wantToEat) {
+			// Don't queue any more of this creature if the opposite creature
+			// wants to eat
 			cv_wait(otherSpecies->doneEating, speciesSwitch);
 		}
+		// Let everyone know that at least one member of my species wants to eat
 		mySpecies->wantToEat = true;
 		while (otherSpecies->numEating > 0) {
 			cv_wait(otherSpecies->doneEating, speciesSwitch);
 		}
+		// Hey everyone, the other species has finished eating!
 		cv_broadcast(otherSpecies->doneEating, speciesSwitch);
+
 	lock_release(speciesSwitch);
 
+	// A member of my species is eating
 	mySpecies->numEating++;
 
 	// Now it's my species turn
-	// Wait until my bowl is available, if required
+	// Wait until the bowl I want to eat from is available, if required
 	lock_acquire(myBowl);
 		while (bowlUsage[bowl - 1]) {
 			cv_wait(myBowlIsAvailable, myBowl);
 		}
-		bowlUsage[bowl - 1] = true;
+		bowlUsage[bowl - 1] = true; // I am now using this bowl
 	lock_release(myBowl);
 
-	P(eating);
-	// Now we eat!
+	P(eating); // Now I eat!
 }
 
+/**
+	Similar to before_eating, but for when a species is done eating
+*/
 void after_eating(struct species *mySpecies, struct species *otherSpecies, unsigned int bowl) {
 
-	V(eating); // done eating
+	V(eating); // I'm done eating, a spot was just freed up
 
-	// done eating, let others wanting to use my bowl know that it's available
+	// Done eating, let others wanting to use my bowl know that it's available
+	// I probably don't need this lock here, but this works so I'm keeping it
 	lock_acquire(myBowl);
 		bowlUsage[bowl - 1] = false;
+		// let anyone waiting to use my bowl know
 		cv_broadcast(myBowlIsAvailable, myBowl);
 	lock_release(myBowl);
 
+	// As far as I'm concerned, my species is done eating
+	// Should this go in a lock or something?
 	mySpecies->wantToEat = false;
 	mySpecies->numEating--;
-	cv_signal(mySpecies->doneEating, speciesSwitch); // anybody else want to eat?
+
+	// anybody else want to eat?
+	cv_signal(mySpecies->doneEating, speciesSwitch);
 
 	if (otherSpecies->wantToEat) {
-		// Wait until this species is fully done eating before letting them sleep
+		// Wait until this species is fully done eating before anyone sleep
 		lock_acquire(speciesSwitch);
 			while (mySpecies->numEating > 0) {
 				cv_wait(mySpecies->doneEating, speciesSwitch);
 			}
+			// Tell the other species that this species is done eating
 			cv_signal(mySpecies->doneEating, speciesSwitch);
 		lock_release(speciesSwitch);
 	}

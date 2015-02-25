f05ec99babdff94de2bf8e5e3367b21bab9f2664
nfrasser <nfrasser@gmail.com>
2015-02-24 22:00:35 -0500

    Initial sys_exit implementation
    Won’t know if this works for sure until we implement waitpid

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -10,6 +10,8 @@
 #include <thread.h>
 #include <addrspace.h>
 #include <copyinout.h>
+#include <synch.h>
+#include <array.h>
 
 /* this implementation of sys__exit does not do anything with the exit code */
 /* this needs to be fixed to get exit() and waitpid() working properly */
@@ -19,15 +21,24 @@ void sys__exit(int exitcode) {
 	struct addrspace *as;
 	struct proc *p = curproc;
 
-	// Signal all the child processes that it is now OKAY to kill themselves
-
-	/* for now, just include this to keep the compiler from complaining about
-		 an unused variable */
-	(void)exitcode;
-
 	DEBUG(DB_SYSCALL,"Syscall: _exit(%d)\n",exitcode);
 
 	KASSERT(curproc->p_addrspace != NULL);
+
+	// Signal all the child processes that it is now OKAY to kill themselves
+	for (unsigned int i = array_num(&p->p_children); i > 0 ; i--) {
+
+		// This allows children to exit
+		struct proc *cproc = array_get(&p->p_children, i - 1);
+		lock_release(cproc->p_exit_lk);
+
+		// Remove the child from the children array
+		array_remove(&p->p_children, i - 1);
+	}
+
+	// All children must have been removed from the array
+	KASSERT(array_num(&p->p_children) == 0);
+
 	as_deactivate();
 	/*
 	 * clear p_addrspace before calling as_destroy. Otherwise if
@@ -43,12 +54,19 @@ void sys__exit(int exitcode) {
 	/* note: curproc cannot be used after this call */
 	proc_remthread(curthread);
 
+	p->p_did_exit = true;
+	p->p_exitcode = exitcode;
+
+	// Let anyone waiting for this thread know that it has exited
+	cv_broadcast(p->p_wait_cv, p->p_wait_lk);
+
 	// Signal parent processes that we're finished!
-	//TODO
 
 	// Okay, at this point, we need to wait for the parent process to exit
 	// before fully destroying ourselves. This way, the parent process can call
 	// waitpid on its children at any time.
+	lock_acquire(p->p_exit_lk);
+	lock_release(p->p_exit_lk);
 
 	/* if this is the last user process in the system, proc_destroy()
 		 will wake up the kernel menu thread */
@@ -108,6 +126,12 @@ int sys_fork(struct trapframe *ctf, pid_t *retval) {
 		return thread_fork_err; // err
 	}
 	DEBUG(DB_SYSCALL, "sys_fork: Current thread forked successfully.\n");
+
+	// Add the child process to the current one's children array
+	array_add(&curp->p_children, newp, NULL);
+
+	// Grab the child's exit lock to prevent it from exiting
+	lock_acquire(newp->p_exit_lk);
 
 	// Return the new processes's ID
 	*retval = newp->p_id;
